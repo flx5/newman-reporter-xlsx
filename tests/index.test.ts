ebd648ab1bf343f04e79e4ebd9defb944b87e5ff
flx5 <1330854+flx5@users.noreply.github.com>
2022-08-04 21:03:05 +0200

    Add test setup

---
+++ tests/index.test.ts
@@ -0,0 +1,9 @@
+import NewmanXslxReporter = require('../src/index');
+import { expect } from 'chai';
+
+describe('NewmanXslxReporter tests', () => {
+    it('TODO', () => {
+       // TODO
+    });
+});
+
